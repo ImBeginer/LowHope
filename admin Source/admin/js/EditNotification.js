64047a78ab6f09b18447e4d366a438d4cffb550b
Vinh Nguyen <vinhnguyenvan1995@gmail.com>
2017-11-30 17:10:10 +0700

    up css

--- admin Source/admin/js/EditNotification.js
+++ admin Source/admin/js/EditNotification.js
@@ -18,5 +18,22 @@ function search() {
 }
 
 $(document).ready(function(){
-	$('#notifi-list').DataTable();
+	$('#notifi-list').DataTable({
+    "language": {
+      "lengthMenu": "Hiển thị _MENU_ bản ghi",
+      "zeroRecords": "Không có dữ liệu",
+      "info": "Trang _PAGE_ trên _PAGES_",
+      "infoEmpty": "Không có bản ghi",
+      "infoFiltered": "(filtered from _MAX_ total records)",
+      "search": "Tìm kiếm:",
+      "loadingRecords": "Đang tải",
+      "processing":     "Đang xử lý",
+      "paginate": {
+        "first":      "Trang đầu",
+        "last":       "Trang cuối",
+        "next":       "Trang kế",
+        "previous":   "Trang trước" 
+      },
+    }        
+  });
 });
