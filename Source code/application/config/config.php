8885fd6df1771c276dbeb3d540d9cc5c028b2af1
duycong2509 <duycong2509@gmail.com>
2017-12-16 00:20:26 +0700

    update code

--- Source code/application/config/config.php
+++ Source code/application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/dgdn/';
+$config['base_url'] = 'http://35.196.25.29/';
 
 /*
 |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'AUTO';
+$config['uri_protocol']	= 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
